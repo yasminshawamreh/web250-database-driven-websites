43c57e6e37dbb9e72ce1fd14da24e2233366e786
Yasmin Shawamreh <yshawamreh@yahoo.com>
2026-09-08 18:13:41 -0500

    Added object access controls

--- asgn02-inheritance/challenge-02-inheritance.php
+++ asgn02-inheritance/challenge-02-inheritance.php
@@ -2,48 +2,69 @@
 
 class Instrument
 {
-    public $name;
-    public $brand;
+    protected $name;
+    private $brand;
 
     public function play()
     {
         echo "The {$this->name} is being played.<br>";
     }
+
+    public function setName($value)
+    {
+        $this->name = $value;
+    }
+
+    public function brand() { 
+        return $this->brand; 
+    } 
+    
+    public function setBrand($value) { 
+        $this->brand = $value; 
+    }
 }
 
 class Guitar extends Instrument
 {
-    public $strings;
+    protected $strings;
 
     public function tune()
     {
         echo "The {$this->name} has {$this->strings} strings and is being tuned.<br>";
     }
+
+    public function setStrings($value) { 
+        $this->strings = $value; 
+    }
 }
 
 class Piano extends Instrument
 {
-    public $keys;
+    protected $keys;
 
     public function tune()
     {
         echo "The {$this->name} has {$this->keys} keys and is being tuned.<br>";
     }
+
+    public function setKeys($value) { 
+        $this->keys = $value; 
+    }
 }
 
 
-// Create a Guitar object
-$guitar = new Guitar();
-$guitar->name = "Guitar";
-$guitar->brand = "Fender";
-$guitar->strings = 6;
+//Create a Guitar object 
+$guitar = new Guitar(); 
+$guitar->setName("Guitar"); 
+$guitar->setBrand("Fender"); 
+$guitar->setStrings(6);
 
 
 // Create a Piano object
 $piano = new Piano();
-$piano->name = "Piano";
-$piano->brand = "Yamaha";
-$piano->keys = 88;
+$piano->setName("Piano");
+$piano->setBrand("Yamaha");
+$piano->setKeys(88);
 
 
 // Demonstrate inherited method
@@ -54,5 +75,9 @@ $piano->play();
 // Demonstrate subclass methods
 $guitar->tune();
 $piano->tune();
+
+// Display brands using getter 
+echo "Guitar brand: " . $guitar->brand() . "<br>"; 
+echo "Piano brand: " . $piano->brand() . "<br>";
 
 ?>
